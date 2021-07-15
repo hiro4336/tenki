eaea714a7d178d56e1c5cb8b64d06fe15e3dc5a6
DESKTOP-AHTTJUL\hiro4 <hiro4mimura@gmail.com>
2021-07-15 17:16:38 +0900

    First / DBファイル

---
+++ sanagi5_data.py
@@ -0,0 +1,45 @@
+import datetime
+# ============================================================================
+# data
+# サナギになった日,チョウの種類/性別,羽化した日
+# ============================================================================
+l=[
+    [datetime.date(2021,4,24),'ナミアゲハ：男', datetime.date(2021,5,4)],
+    [datetime.date(2021,4,25),'ナミアゲハ：女', datetime.date(2021,5,7)],
+    [datetime.date(2021,4,28),'ナミアゲハ：男', datetime.date(2021,5,8)],
+    [datetime.date(2021,4,28),'ナミアゲハ：男', datetime.date(2021,5,8)],
+    [datetime.date(2021,4,28),'ナミアゲハ：女', datetime.date(2021,5,8)],
+
+    [datetime.date(2021,4,29),'ナミアゲハ：女', datetime.date(2021,5,9)],
+    [datetime.date(2021,4,30),'ナミアゲハ：男', datetime.date(2021,5,10)],
+    [datetime.date(2021,4,30),'ナミアゲハ：男', datetime.date(2021,5,10)],
+    [datetime.date(2021,5,9) ,'ナミアゲハ：女', datetime.date(2021,5,20)],
+    [datetime.date(2021,5,12),'ナミアゲハ：里子へ',0],
+
+    [datetime.date(2021,5,13),'ナミアゲハ：里子へ',0],
+    [datetime.date(2021,5,14),'ナミアゲハ：男',  datetime.date(2021,5,25)],
+    [datetime.date(2021,5,14),'ナミアゲハ：男',  datetime.date(2021,5,22)],
+    [datetime.date(2021,5,15),'クロアゲハ：男',  datetime.date(2021,5,27)],
+    [datetime.date(2021,5,17),'キアゲハ1号：男', datetime.date(2021,5,27)],
+
+    [datetime.date(2021,5,22),'ジャコウアゲハ1号：男', datetime.date(2021,6,2)],
+    [datetime.date(2021,5,22),'キアゲハ2号：', datetime.date(2021,6,1)],
+    [datetime.date(2021,5,23),'キアゲハ3号：', datetime.date(2021,6,2)],
+    [datetime.date(2021,5,29),'ナガサキアゲハ：',datetime.date(2021,6,10)],
+    [datetime.date(2021,5,29),'ジャコウアゲハ：',0],
+
+    [datetime.date(2021,5,30),'ジャコウアゲハ：',0],
+    [datetime.date(2021,6,5),'ナミアゲハ：男',datetime.date(2021,6,14)],
+    [datetime.date(2021,6,5),'ナミアゲハ：女',datetime.date(2021,6,15)],
+    [datetime.date(2021,6,5),'ナミアゲハ：里子へ',0],
+    [datetime.date(2021,6,5),'ナミアゲハ：里子へ',0],
+
+    [datetime.date(2021,6,9),'クロアゲハ',datetime.date(2021,6,21)],
+    [datetime.date(2021,6,12),'ツマグロヒョウモン',datetime.date(2021,6,20)],
+    [datetime.date(2021,6,12),'ツマグロヒョウモン',datetime.date(2021,6,20)],
+    [datetime.date(2021,6,3),'オオスカシバ',datetime.date(2021,6,17)],
+    [datetime.date(2021,6,4),'オオスカシバ',datetime.date(2021,6,18)],
+    [datetime.date(2021,7,1),'アオスジアゲハ',0],
+    [datetime.date(2021,7,4),'アオスジアゲハ',0]
+    ]
+
